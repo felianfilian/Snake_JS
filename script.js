a03d911a31d360b190f13b7ce0a30997ee57d134
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-02-20 00:01:51 +0100

    first graphics

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert('snake');
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
+ctx.fillStyle = 'white';
+ctx.fillRect(100, 100, 20 - 1, 20 - 1);
+ctx.fillRect(120, 100, 20 - 1, 20 - 1);
+ctx.fillRect(140, 100, 20 - 1, 20 - 1);
+ctx.fillRect(160, 120, 20 - 1, 20 - 1);
+ctx.fillStyle = 'green';
+ctx.fillRect(200, 200, 20 - 1, 20 - 1);
