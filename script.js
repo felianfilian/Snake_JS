cfdcb562ee7619cb613c87431276c1ddc3e39c35
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-02-20 00:03:57 +0100

    basic functions

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'black';
-ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
-ctx.fillStyle = 'white';
-ctx.fillRect(100, 100, 20 - 1, 20 - 1);
-ctx.fillRect(120, 100, 20 - 1, 20 - 1);
-ctx.fillRect(140, 100, 20 - 1, 20 - 1);
-ctx.fillRect(160, 120, 20 - 1, 20 - 1);
-ctx.fillStyle = 'green';
-ctx.fillRect(200, 200, 20 - 1, 20 - 1);
+draw()
+
+function draw() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(100, 100, 20 - 1, 20 - 1);
+    ctx.fillRect(120, 100, 20 - 1, 20 - 1);
+    ctx.fillRect(140, 100, 20 - 1, 20 - 1);
+    ctx.fillRect(160, 120, 20 - 1, 20 - 1);
+    ctx.fillStyle = 'green';
+    add(200, 300);
+
+}
+
+function add(x, y) {
+    ctx.fillRect(x, y, 20 - 1, 20 - 1);
+}
+
+
