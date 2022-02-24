804af4e8dcf1a62d9d6ee59d350114fd32189f9a
Dennis Schmalstieg <darianbellentor@outlook.de>
2022-02-24 18:11:26 +0100

    graphic placing logic

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 
+let rows = 20;
+let cols = 20;
+let celWidth = canvas.width / cols;
+let celHeight = canvas.height / rows;
+
 draw()
 
 function draw() {
     ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.clientWidth, canvas.height);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = 'white';
-    ctx.fillRect(100, 100, 20 - 1, 20 - 1);
-    ctx.fillRect(120, 100, 20 - 1, 20 - 1);
-    ctx.fillRect(140, 100, 20 - 1, 20 - 1);
-    ctx.fillRect(160, 120, 20 - 1, 20 - 1);
+    add(2, 3);
+    add(3, 3);
+    add(4, 3);
+    add(4, 4);
     ctx.fillStyle = 'green';
-    add(200, 300);
+    add(8, 8);
 
 }
 
 function add(x, y) {
-    ctx.fillRect(x, y, 20 - 1, 20 - 1);
+    ctx.fillRect(x * celWidth, y * celHeight, celWidth - 1, celHeight - 1);
 }
 
 
